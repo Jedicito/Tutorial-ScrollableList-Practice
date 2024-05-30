358b141bfbcdcdd23f169aa8a31dbef58c954574
Jedicito <seron.cesar@gmail.com>
2024-05-29 23:22:07 -0400

    Data class Curso  y datasource definidos. Faltan imagenes.

---
+++ app/src/main/java/chl/ancud/tutorialscrollablelistpractice/model/Curso.kt
@@ -0,0 +1,11 @@
+package chl.ancud.tutorialscrollablelistpractice.model
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+
+data class Curso (
+    @StringRes val stringResTipoCurso: Int,
+    val cuentaCursos: Int,
+    @DrawableRes val drawableRes: Int,
+
+)
